029e6db9e50de6d3dc340b307cdef6ba56ed81f4
minnionmonkey <zackarye.bates@gmail.com>
2026-09-04 22:57:11 -0600

    Ok, now Week 1 should be done. Hopefully. Please.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,4 +4,4 @@ const lastModified = document.querySelector("#lastModified")
 const today = new Date();
 
 currentYear.textContent = today.getFullYear();
-document.getElementById("lastModified").textContent = 'Last Modified: ${document.lastModified}';
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
