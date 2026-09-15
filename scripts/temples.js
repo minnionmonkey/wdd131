45a0193e11dd7444a6c8cfad711402e81edb247c
minnionmonkey <zackarye.bates@gmail.com>
2026-09-14 21:16:46 -0600

    Added the hamburger menu for temples.html

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,15 @@
 const currentYear = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified")
+const lastModified = document.querySelector("#lastModified");
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
 
 const today = new Date();
 
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
 currentYear.textContent = today.getFullYear();
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
